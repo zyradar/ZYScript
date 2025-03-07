a26b96b43486d7fc5f806e58a5f465fc53319c9c
HZY <1795438624@qq.com>
2025-03-08 04:10:10 +0800

    上传URL列表

---
+++ GetURL.py
@@ -0,0 +1,11 @@
+UI_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/UI.py'
+GetParameters_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/GetParameters.py'
+CopyPaste_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/CopyPaste.py'
+EXpandData_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/EXpandData.py'
+ConvertFormat_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/ConvertFormat.py'
+MakeDataSet_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/MakeDataSet.py'
+CheckData_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/CheckData.py'
+Crawler_URL = 'https://raw.githubusercontent.com/zyradar/ZYScript/TestScritpt/Crawler.py'
+CODE_URL = {'GetParameters': GetParameters_URL, 'CopyPaste': CopyPaste_URL, 'EXpandData': EXpandData_URL,
+            'ConvertFormat': ConvertFormat_URL, 'MakeDataSet': MakeDataSet_URL, 'CheckData': CheckData_URL,
+            'Crawler': Crawler_URL, 'UI': UI_URL}
